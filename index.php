253f233e83641f0e2efe097bca96898e2f0705e6
Jake Taylor <me@jaketaylor.co>
2017-10-08 12:35:45 +0100

    hardcoded url to bypass eddystone restrictions quickly

--- index.php
+++ index.php
@@ -11,6 +11,14 @@ Flight::route('GET /@animal:[A-Fa-f0-9]{24}', [
     'show',
 ]);
 
+// TEMP
+
+Flight::route('GET /el', function () {
+    Flight::render('animals/show', [
+        'api_url' => 'https://example.com/',
+    ]);
+});
+
 // Set Views Directory
 Flight::set('flight.views.path', './php/views');
 
